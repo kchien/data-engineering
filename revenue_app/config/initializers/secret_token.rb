513df98640f9aae837f34a6d0b276d9d5acde31b
Kenrick Chien <ken.chien@gmail.com>
2013-12-17 11:26:16 -0500

    generate rails app

---
+++ revenue_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RevenueApp::Application.config.secret_key_base = '287a67c6ab5ccdef9445f8d3f5c0c3a6c3eaee8c08ef805fcdc837288cfe7a32f8de479e941044063befd6e1174dcdddd00031de4767528d655c7f71024bef6a'
---
+++ revenue_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RevenueApp::Application.config.session_store :cookie_store, key: '_revenue_app_session'
