b85d8c91925bdfa55da7f62c1eec6dc1bed7607b
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 11:26:35 -0500

    handle multiple lines

--- revenue_app/spec/models/revenue_calculator_spec.rb
+++ revenue_app/spec/models/revenue_calculator_spec.rb
@@ -12,7 +12,7 @@ describe RevenueCalculator do
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.0	1	987 Fake St	Bob's Pizza}
     }
-    
+
     When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
 
     Then {
@@ -42,7 +42,23 @@ Amy Pond	$30 of awesome for $10	10.0	5	456 Unreal Rd	Tom's Awesome Shop}
     }
 
     When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
-   
+
     Then { revenue.should == 70.0 }
+  end
+
+  context "orders file has lines with duplicate merchants and customers" do
+    Given(:orders_file) {
+%Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
+Snake Plissken	$10 off $20 of food	10.0	2	987 Fake St	Bob's Pizza
+Amy Pond	$30 of awesome for $10	10.0	5	456 Unreal Rd	Tom's Awesome Shop
+Marty McFly	$20 Sneakers for $5	5.0	1	123 Fake St	Sneaker Store Emporium
+Snake Plissken	$20 Sneakers for $5	5.0	4	123 Fake St	Sneaker Store Emporium}
+    }
+    When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+
+    Then { revenue.should == 95.0 }
+  end
+
+  context "orders file has dollars and cents" do
   end
 end
