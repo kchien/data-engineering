95548de8a6112be9e995c75990dd2a866c170a20
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 17:02:02 -0500

    Prevent duplicate customers from being created

--- revenue_app/spec/models/customer_spec.rb
+++ revenue_app/spec/models/customer_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 describe Customer do
- context "setting the name" do
-   it "persists the name" do
-     chuck = 'Chuck Norris'
-     customer = described_class.new(name: chuck)
-     customer.name.should == chuck
-   end
- end
+  let(:chuck) { 'Chuck Norris' }
+  let(:chuck_attrs) { {name: chuck} }
+
+  context "setting the name" do
+    it "persists the name" do
+      customer = described_class.new(name: chuck)
+      customer.name.should == chuck
+    end
+  end
+
+  context "attemping to create duplicate customers" do
+    it "does not allow customers with the same name to be created" do
+      customer = described_class.create(chuck_attrs)
+      expect {
+        described_class.create!(chuck_attrs)
+      }.to raise_error
+    end
+  end
 end
