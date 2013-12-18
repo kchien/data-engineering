67988a5ef9d055c22379292892dbd02f2013c9d8
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 13:24:14 -0500

    Change to read from file

--- revenue_app/app/models/revenue_calculator.rb
+++ revenue_app/app/models/revenue_calculator.rb
@@ -1,7 +1,7 @@
 require 'csv'
 class RevenueCalculator
   def calculate_revenue_from_file(file)
-    csv = CSV.parse(file, headers:true, col_sep: "\t", skip_blanks: true, converters: :numeric)
+    csv = CSV.read(file, headers:true, col_sep: "\t", skip_blanks: true, converters: :numeric)
 
     csv.reduce(0) {|revenue, row|
       revenue + row["item price"] * row["purchase count"]
--- revenue_app/spec/models/revenue_calculator_spec.rb
+++ revenue_app/spec/models/revenue_calculator_spec.rb
@@ -7,65 +7,81 @@ require 'pry'
 describe RevenueCalculator do
   Given(:calculator) { described_class.new }
 
+  Given(:full_path_to_file) {
+    filename = "sample_input.tab"
+    write_file(filename, file_contents)
+    File.join(current_dir, filename)
+  }
+
   context "orders file has only one line of data where customer bought one item" do
-    Given(:orders_file) {
+    Given(:file_contents) {
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.0	1	987 Fake St	Bob's Pizza}
     }
 
-    When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+    When(:revenue) {
+      calculator.calculate_revenue_from_file(full_path_to_file)
+    }
 
     Then {
       revenue.should == 10.0
     }
 
     context "customer bought two (2) of the same item" do
-      Given(:orders_file) {
+      Given(:file_contents) {
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.0	2	987 Fake St	Bob's Pizza}
       }
 
-      When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+      When(:revenue) {
+       calculator.calculate_revenue_from_file(full_path_to_file)
+      }
 
       Then {
         revenue.should == 20.0
       }
-
     end
   end
 
   context "orders file has two lines of data from two different customers" do
-    Given(:orders_file) {
+    Given(:file_contents) {
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.0	2	987 Fake St	Bob's Pizza
 Amy Pond	$30 of awesome for $10	10.0	5	456 Unreal Rd	Tom's Awesome Shop}
     }
 
-    When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+    When(:revenue) {
+     calculator.calculate_revenue_from_file(full_path_to_file)
+    }
 
     Then { revenue.should == 70.0 }
   end
 
   context "orders file has lines with duplicate merchants and customers" do
-    Given(:orders_file) {
+    Given(:file_contents) {
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.0	2	987 Fake St	Bob's Pizza
 Amy Pond	$30 of awesome for $10	10.0	5	456 Unreal Rd	Tom's Awesome Shop
 Marty McFly	$20 Sneakers for $5	5.0	1	123 Fake St	Sneaker Store Emporium
 Snake Plissken	$20 Sneakers for $5	5.0	4	123 Fake St	Sneaker Store Emporium}
     }
-    When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+
+    When(:revenue) {
+     calculator.calculate_revenue_from_file(full_path_to_file)
+    }
 
     Then { revenue.should == 95.0 }
   end
 
   context "orders file has dollars and cents" do
-    Given(:orders_file) {
+    Given(:file_contents) {
 %Q{purchaser name	item description	item price	purchase count	merchant address	merchant name
 Snake Plissken	$10 off $20 of food	10.33	2	987 Fake St	Bob's Pizza}
     }
 
-    When(:revenue) { calculator.calculate_revenue_from_file(orders_file) }
+    When(:revenue) {
+     calculator.calculate_revenue_from_file(full_path_to_file)
+    }
 
     Then { revenue.should == 20.66 }
   end
