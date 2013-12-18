eb4eefb4a18eff31cd2d7f6da4ae8f89a9d563e9
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 16:30:24 -0500

    ensure merchant has many items

--- revenue_app/spec/models/merchant_spec.rb
+++ revenue_app/spec/models/merchant_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 describe Merchant do
+ it { should have(0).items }
+ 
  context "setting its default attributes" do
    it "has an address and a name" do
      address = '123 Fake St.'
@@ -7,6 +9,16 @@ describe Merchant do
      merchant = described_class.create(address: address, name: name)
      expect(merchant.address).to eq(address)
      expect(merchant.name).to eq(name)
+   end
+ end
+
+
+ context "after adding some items" do
+   it "contains those items" do
+     quick_e_mart = described_class.create(address: "Foo St.", name: 'Quick-E-Mart')
+     milk = quick_e_mart.items.create(description: "Milk", price: 0.25)
+
+     expect(quick_e_mart.items).to eq([milk])
    end
  end
 end
