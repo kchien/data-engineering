f7f03ec77d896747970d15c937d78492126512ef
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 15:41:40 -0500

    cuke passes

---
+++ revenue_app/app/controllers/revenues_controller.rb
@@ -0,0 +1,67 @@
+class RevenuesController < ApplicationController
+  before_action :set_revenue, only: [:show, :edit, :update, :destroy]
+
+  # GET /revenues
+  # GET /revenues.json
+  def index
+    @revenues = Revenue.all
+  end
+
+  # GET /revenues/1
+  # GET /revenues/1.json
+  def show
+  end
+
+  # GET /revenues/new
+  def new
+    @revenue = Revenue.new
+  end
+
+  # GET /revenues/1/edit
+  def edit
+  end
+
+  # POST /revenues
+  # POST /revenues.json
+  def create
+    r = Revenue.create(revenue_params)
+    file = r.purchases_file.current_path
+    RevenueCalculator.new.calculate_revenue_from_file(file, r)
+    redirect_to revenues_url
+  end
+
+  # PATCH/PUT /revenues/1
+  # PATCH/PUT /revenues/1.json
+  def update
+    respond_to do |format|
+      if @revenue.update(revenue_params)
+        format.html { redirect_to @revenue, notice: 'Revenue was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @revenue.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /revenues/1
+  # DELETE /revenues/1.json
+  def destroy
+    @revenue.destroy
+    respond_to do |format|
+      format.html { redirect_to revenues_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_revenue
+      @revenue = Revenue.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def revenue_params
+      params.require(:revenue).permit(:purchases_file)
+    end
+end
--- revenue_app/features/step_definitions/calculate_revenue_steps.rb
+++ revenue_app/features/step_definitions/calculate_revenue_steps.rb
@@ -1,9 +1,14 @@
 Given(/^I have a file named "(.*?)" with the following content:$/) do |file_name, file_content|
-  write_file(file_name, file_content)
+  @file_name = file_name
+  write_file(@file_name, file_content)
 end
 
 When(/^I upload the revenue file$/) do
   visit new_revenue_path
+  within("#new_revenue") do
+    attach_file("revenue_purchases_file", File.join(current_dir, @file_name))
+  end
+  click_button("submit")
 end
 
 Then(/^I should see a gross revenue of \$(\d+)$/) do |revenue|
