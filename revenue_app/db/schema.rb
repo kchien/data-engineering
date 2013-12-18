bff5dab74e32516a1f96c7da713687c8e505fbbf
Kenrick Chien <ken.chien@gmail.com>
2013-12-18 13:35:43 -0500

    revenue route & migration

--- revenue_app/db/schema.rb
+++ revenue_app/db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131217174245) do
+
+  create_table "revenues", force: true do |t|
+    t.decimal  "total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- revenue_app/spec/spec_helper_lite.rb
+++ revenue_app/spec/spec_helper_lite.rb
@@ -1,7 +1,13 @@
+require 'aruba/api'
+
 def needs(path)
   current_directory = File.dirname(__FILE__)
   full_path = File.expand_path(current_directory + '/../' + path)
   unless $:.include?(full_path)
     $: << full_path
   end
+end
+
+RSpec.configure do |c|
+  c.include Aruba::Api
 end
