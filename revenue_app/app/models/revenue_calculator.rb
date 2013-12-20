11a89c047e1168e4b8f3b5e266388081626eaa3b
Kenrick Chien <ken.chien@gmail.com>
2013-12-20 00:18:57 -0500

    refactor

--- revenue_app/app/models/revenue_calculator.rb
+++ revenue_app/app/models/revenue_calculator.rb
@@ -1,10 +1,16 @@
 require 'csv'
+
 class RevenueCalculator
   def calculate_revenue_from_file(file, revenue_for_file)
     csv = CSV.read(file, headers:true, col_sep: "\t", skip_blanks: true, converters: :numeric)
 
-    revenue_for_file.update_total_to csv.reduce(0) {|revenue, row|
+    revenue_for_file.update_total_to(sum_of_all_revenues_in(csv))
+  end
+
+  def sum_of_all_revenues_in csv
+    csv.reduce(0) {|revenue, row|
       revenue + row["item price"] * row["purchase count"]
     }
   end
+
 end
