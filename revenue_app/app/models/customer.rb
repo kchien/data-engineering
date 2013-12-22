459c35ad4c1ec2a57a94ffcda2c13db5745c52bf
Kenrick Chien <ken.chien@gmail.com>
2013-12-22 00:32:11 -0500

    whitespace

--- revenue_app/app/models/customer.rb
+++ revenue_app/app/models/customer.rb
@@ -7,12 +7,12 @@ class Customer < ActiveRecord::Base
   def self.store_purchases(csv)
     csv.map do |c|
       Customer.find_or_create_by(name: c["purchaser name"]).tap { |customer|
-      m = Merchant.find_or_create_by(name: c["merchant name"],
-	                     address: c["merchant address"])
-      item = m.items.find_or_create_by(description: c["item description"],
-                              price: c["item price"])
-      item.purchases.create(customer: customer,
-                            quantity:c["purchase count"])
+        m = Merchant.find_or_create_by(name: c["merchant name"],
+  	                     address: c["merchant address"])
+        item = m.items.find_or_create_by(description: c["item description"],
+                                price: c["item price"])
+        item.purchases.create(customer: customer,
+                              quantity:c["purchase count"])
       
       }
     end
